e88bfc144ed931dcfbe4f0f848e7b24d12783208
ibrarian9 <ibrarian9@gmail.com>
2025-01-12 19:42:30 +0700

    Update Detail in Favorite

--- favorite/src/main/java/com/app/expertsubmissionapp/favorite/presentation/favorite/FavoriteFragment.kt
+++ favorite/src/main/java/com/app/expertsubmissionapp/favorite/presentation/favorite/FavoriteFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.expertsubmissionapp.R
 import com.app.expertsubmissionapp.core.adapter.ProductsAdapter
 import com.app.expertsubmissionapp.favorite.databinding.FragmentFavoriteBinding
 import com.app.expertsubmissionapp.favorite.di.favoriteModule
@@ -36,6 +38,14 @@ class FavoriteFragment : Fragment() {
             rv.apply {
                 layoutManager = LinearLayoutManager(requireActivity())
                 adapter = productsAdapter
+            }
+
+            productsAdapter.onItemClick = {
+                val bundle =
+                    Bundle().apply {
+                        putInt("productId", it.id)
+                    }
+                findNavController().navigate(R.id.navigation_detail, bundle)
             }
         }
     }
